b880486e4a4e99dc80ea1a12f4cc1bfd4312aabd
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-08-24 18:11:20 +0700

    feat: replace default componetns

--- src/components/pricing/index.tsx
+++ src/components/pricing/index.tsx
@@ -1,3 +1,4 @@
+import { SectionTitle } from "../section";
 import { tiers } from "./constants";
 import PricingCard from "./PricingCard";
 
@@ -5,18 +6,10 @@ function Pricing() {
   return (
     <section className="bg-white pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] relative z-20 overflow-hidden">
       <div className="container">
-        <div className="flex flex-wrap -mx-4">
-          <div className="w-full px-4">
-            <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-              <h2 className=" font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 ">
-                Our Pricing Plan
-              </h2>
-              <p className="text-base text-body-color">
-                Pricing built for all businesses
-              </p>
-            </div>
-          </div>
-        </div>
+        <SectionTitle
+          title="Our Pricing Plan"
+          description="Pricing built for all businesses"
+        />
         <div className="flex flex-wrap justify-center -mx-4">
           {tiers.map((tier, index) => (
             <PricingCard tier={tier} key={index} />
