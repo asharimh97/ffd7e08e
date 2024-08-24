30bbd9ee3e61b133d36575ee480b7de3594e21e3
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-08-24 18:11:03 +0700

    feat: add navbar

---
+++ src/components/header/Navigation.tsx
@@ -0,0 +1,36 @@
+import { GrTechnology } from "react-icons/gr";
+
+function Navigation() {
+  return (
+    <nav id="top-navigation" className="flex justify-between items-center">
+      <a href="/" className="flex gap-3 items-center">
+        <GrTechnology />
+        <span className="hidden font-bold text-black md:block">
+          Tech Solution
+        </span>
+      </a>
+
+      <div className="flex gap-4 items-center md:gap-6">
+        <ul className="flex gap-4 md:gap-6 text-sm">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Pricing</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <a
+          href="/"
+          className="border border-indigo-600 bg-indigo-700 text-white rounded-md py-1 px-6 font-semibold text-sm"
+        >
+          Login
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
